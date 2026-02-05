595bc04daf42a7ba62dada045694262b4f5748db
Oriol Colomés <oriol.colomes@gmail.com>
2026-02-05 15:38:22 +0100

    Update authors and repository links in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,12 @@ DocMeta.setdocmeta!(WaveSpec, :DocTestSetup, :(using WaveSpec); recursive=true)
 
 makedocs(;
     modules=[WaveSpec],
-    authors="Shagun Agarwal shagun.1994@gmail.com, Thomas Kluwer, Oriol Colomes",
-    repo="https://github.com/shagunTUD/WaveSpec.jl/blob/{commit}{path}#{line}",
+    authors="Shagun Agarwal, Pau Manyer, Oriol Colomes",
+    repo="https://github.com/CMOE-TUDelft/WaveSpec.jl/blob/{commit}{path}#{line}",
     sitename="WaveSpec.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://shagunTUD.github.io/WaveSpec.jl",
+        canonical="https://CMOE-TUDelft.github.io/WaveSpec.jl",
         edit_link="main",
         assets=String[],
     ),
@@ -21,6 +21,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/shagunTUD/WaveSpec.jl",
+    repo="github.com/CMOE-TUDelft/WaveSpec.jl",
     devbranch="main",
 )
